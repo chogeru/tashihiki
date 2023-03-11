0afbb94fa02afc1f3c6423217bfaa4325c9248f7
SAkana <d.anabuki-college.net>
2023-03-11 15:08:01 +0900

    tuika

---
+++ タシヒキ/Project/GameProject/Title/Title.cpp
@@ -0,0 +1,24 @@
+#include "Title.h"
+
+Title::Title():Base(eType_Scene)
+{
+}
+
+Title::~Title()
+{
+	//KillAll();
+	//Base::Add(new Game());
+}
+
+void Title::Updata()
+{//ƒ{ƒ^ƒ“‚P‚Å”jŠü
+	if (PUSH(CInput::eButton1)) {
+		m_kill = true;
+	}
+}
+
+void Title::Draw()
+{
+	m_img.Draw();
+	m_img.SetSize(1920, 1080);
+}
--- タシヒキ/Project/GameProject/h.h
+++ タシヒキ/Project/GameProject/h.h
@@ -1,2 +1,3 @@
 #pragma once
 //ここでヘッダーをインクルードする
+//#include "UI/Title.h"
